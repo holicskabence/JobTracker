ffbad4762eb27ca70d143bb17d821885570faa49
holicskabence <101716648+holicskabence@users.noreply.github.com>
2026-06-13 16:23:41 +0200

    Fix gaps in overview page

--- JobTracker.WebApi/obj/Release/net9.0/JobTracker.WebApi.AssemblyInfo.cs
+++ JobTracker.WebApi/obj/Release/net9.0/JobTracker.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JobTracker.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+283040651f6b093a512e5c481044d31938f3a32b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d01c3b48765cb0f3686d80cd73532adcba8bf634")]
 [assembly: System.Reflection.AssemblyProductAttribute("JobTracker.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JobTracker.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
