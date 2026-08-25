f4dbfce1f655ecf41dcb0a8d560f907a94bcb6cf
holicskabence <101716648+holicskabence@users.noreply.github.com>
2026-08-25 22:22:40 +0200

    Add new robot image

--- JobTracker.Angular/src/app/components/landing-v2/dashboard-preview/dashboard-preview.component.ts
+++ JobTracker.Angular/src/app/components/landing-v2/dashboard-preview/dashboard-preview.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { DashboardMockupComponent } from './dashboard-mockup.component';
+import { CtaBannerComponent } from "../cta-banner/cta-banner.component";
 
 @Component({
   selector: 'app-landing-v2-dashboard-preview',
   standalone: true,
-  imports: [RouterLink, TranslateModule, DashboardMockupComponent],
+  imports: [RouterLink, TranslateModule, DashboardMockupComponent, CtaBannerComponent],
   templateUrl: './dashboard-preview.component.html',
   styleUrl: './dashboard-preview.component.css'
 })
